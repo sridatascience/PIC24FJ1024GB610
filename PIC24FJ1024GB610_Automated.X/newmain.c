9f5544e0b5fbb8545f1bc9d11071704320c0dffd
sridhar vumma <50115187+sridatascience@users.noreply.github.com>
2023-04-26 11:35:58 +0530

    modified the scripts

--- PIC24FJ1024GB610_Automated.X/newmain.c
+++ PIC24FJ1024GB610_Automated.X/newmain.c
@@ -10,17 +10,19 @@
 
 #include <xc.h>
 #pragma config ICS = PGD2               // ICD Communication Channel Select bits (Communicate on PGEC2 and PGED2)
-uint8_t m =10;
-uint8_t add(uint8_t n, uint8_t o, uint8_t p ){
+volatile uint8_t m =10;
+uint8_t add(volatile uint8_t n, volatile uint8_t o, volatile uint8_t p ){
    return n+o+p;
 }
 void main() {
-   uint8_t a = 0x05;
-   LATA = a;
-   uint8_t c = 0x1F;
-   TRISA = c;
-   uint8_t b =0x08;
-   LATB = b;
-   uint8_t t = add(a,b,c);
+   volatile uint8_t a = 0x05;
+   TMR1 = a;
+   m=TMR1;
+   volatile uint8_t c = 0x1F;
+   TMR2 = c;
+   volatile uint8_t b =0x08;
+   TMR3 = b;
+   volatile uint8_t t = add(a,b,c);
+   TMR4= t;
    return;
 }
